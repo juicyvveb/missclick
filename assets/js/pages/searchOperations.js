4aebef41b6d6b94ee25e57df3034c6ec50f08358
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-02-24 17:51:52 +0300

    operations page

--- assets/js/pages/searchOperations.js
+++ assets/js/pages/searchOperations.js
@@ -1,9 +1,31 @@
-// var table = new DataTable('#fuckTable', {
-
-// })
-
-console.log(document.querySelector("#fuck"));
 document.addEventListener("DOMContentLoaded", function () {
-  new DataTable("#fuck", { scrollX: !0 });
-});
+  
+  const warnText = document.querySelector('.noresult');
+  const selectSystem = document.querySelector('#SelectPayMentSystem');
+  const selectStatus = document.querySelector('#SelectStatusOperation');
+  const searchInput = document.querySelector('#operations-search');
+  const options = {
+    valueNames: ['currency_name','s_operation', 's_system', 's_coming', "s_coming", "s_spending", "s_batch", "s_data", "s_status", "s_description", "s_action"]
+  };
 
+  let list = new List('operations', options);
+
+
+  searchInput.addEventListener('input', (e) => {
+    list.search(e.target.value)
+  })
+
+
+  selectSystem.addEventListener('input', (e) => {
+    list.filter((item) =>  item.values().currency_name.toLowerCase().includes(e.target.value) && item.values().s_status.toLowerCase().includes(selectStatus.value))
+  })
+
+  selectStatus.addEventListener('input', (e) => {
+    list.filter((item) =>  item.values().s_status.toLowerCase().includes(e.target.value) && item.values().currency_name.toLowerCase().includes(selectSystem.value))
+  })
+
+
+  list.on('updated', (e) => {
+    !e.matchingItems.length ? warnText.style.display = "block" : warnText.style.display = "none"
+  })
+});
