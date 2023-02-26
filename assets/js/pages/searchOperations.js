50c4c7d7b3e7e5e2ecd137248aa37f2d5dc83761
juicyvveb <luceriy87@mail.ru>
2023-02-26 23:39:14 +0300

    old version before deleted

--- assets/js/pages/searchOperations.js
+++ assets/js/pages/searchOperations.js
@@ -1,31 +1,102 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
   const warnText = document.querySelector('.noresult');
-  const selectSystem = document.querySelector('#SelectPayMentSystem');
-  const selectStatus = document.querySelector('#SelectStatusOperation');
   const searchInput = document.querySelector('#operations-search');
+  const form = document.querySelector('#operationsSearchForm');
   const options = {
-    valueNames: ['currency_name','s_operation', 's_system', 's_coming', "s_coming", "s_spending", "s_batch", "s_data", "s_status", "s_description", "s_action"]
+    valueNames: ['currency_name','s_operation', 's_coming', "s_spending", "s_batch", "s_data", "s_status", "s_description", "s_action"],
+    page: 10,
+      pagination: [
+        {
+          paginationClass: 'listjs-pagination', 
+          outerWindow: 2
+        }
+      ]
   };
-
+  let searchText = "", statusValue = "", systemValue = "";
+  const filterBtn = document.querySelector('#operation-filter')
   let list = new List('operations', options);
-
-
+  
   searchInput.addEventListener('input', (e) => {
-    list.search(e.target.value)
+    searchText = e.target.value.toLowerCase();
+    // list.filter((item) => filter(item, searchInput, selectSystem, selectStatus))
   })
 
-
-  selectSystem.addEventListener('input', (e) => {
-    list.filter((item) =>  item.values().currency_name.toLowerCase().includes(e.target.value) && item.values().s_status.toLowerCase().includes(selectStatus.value))
+  const choiceStatus = new Choices('#SelectStatusOperation', {
+    searchEnabled: false,
+    labelId: 'status-operation-select',
+  });
+  choiceStatus.passedElement.element.addEventListener('choice', (e) => {
+    statusValue = e.detail.choice.value;
   })
 
-  selectStatus.addEventListener('input', (e) => {
-    list.filter((item) =>  item.values().s_status.toLowerCase().includes(e.target.value) && item.values().currency_name.toLowerCase().includes(selectSystem.value))
+  const choiceSystem = new Choices('#SelectPayMentSystem', {
+    searchEnabled: false,
+    labelId: 'system-operation-select',
+  });
+  choiceSystem.passedElement.element.addEventListener('choice', (e) => {
+    systemValue = e.detail.choice.value;
   })
+
+  // selectSystem.addEventListener('input', (e) => {
+  //   list.filter((item) =>  filter(item))
+  // })
+
+  // selectStatus.addEventListener('input', (e) => {
+  //   list.filter((item) =>  filter(item))
+  // })
 
 
   list.on('updated', (e) => {
-    !e.matchingItems.length ? warnText.style.display = "block" : warnText.style.display = "none"
+    !e.matchingItems.length ? warnText.style.display = "block" : warnText.style.display = "none";
+
+    // remove pagination by nothing matched
+    e.matchingItems.length ? document.querySelector(`#operations .pagination-wrap`).style.display = "flex" : document.querySelector(`#operations .pagination-wrap`).style.display = "none"
   })
+
+  filterBtn.onclick = () => {
+    
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log('submit')
+    list.filter(item => {
+      return (item.values().currency_name.toLowerCase().includes(searchText) 
+    || item.values().s_operation.toLowerCase().includes(searchText)
+    ||  item.values().s_coming.toLowerCase().includes(searchText)
+    ||  item.values().s_spending.toLowerCase().includes(searchText)
+    ||  item.values().s_batch.toLowerCase().includes(searchText)
+    ||  item.values().s_status.toLowerCase().includes(searchText)
+    ||  item.values().s_description.toLowerCase().includes(searchText)
+    ||  item.values().s_action.toLowerCase().includes(searchText)
+    ) && item.values().s_status.toLowerCase().includes(statusValue) && item.values().currency_name.toLowerCase().includes(systemValue)
+  })  
+  })
+  // function filter(item, search = searchInput, system = selectSystem, status = selectStatus){
+  //   return (
+  //     item.values()['currency_name'].toLowerCase().includes(search.value.toLowerCase()) || item.values()['s_operation'].toLowerCase().includes(search.value.toLowerCase())
+  //   ) && (item.values().s_status.toLowerCase().includes(status.value.toLowerCase()) && item.values().currency_name.toLowerCase().includes(system.value.toLowerCase()))
+  // }
 });
+
+
+// pagination prev and next
+document.querySelector(".pagination-next").addEventListener("click", function () {
+  if(document.querySelector(".pagination.listjs-pagination").querySelector(".active").nextElementSibling){
+    // document.querySelector(".pagination.listjs-pagination") && document.querySelector(".pagination.listjs-pagination").querySelector(".active") && document.querySelector(".pagination.listjs-pagination").querySelector(".active").nextElementSibling.children[0].click()
+     document.querySelector(".pagination.listjs-pagination").querySelector(".active").nextElementSibling.children[0].click()
+    return
+  }
+})
+
+document.querySelector(".pagination-prev").addEventListener("click", function () {
+  if(document.querySelector(".pagination.listjs-pagination").querySelector(".active").previousSibling){
+    // document.querySelector(".pagination.listjs-pagination") && document.querySelector(".pagination.listjs-pagination").querySelector(".active") && document.querySelector(".pagination.listjs-pagination").querySelector(".active").previousSibling.children[0].click()
+    document.querySelector(".pagination.listjs-pagination").querySelector(".active").previousSibling.children[0].click()
+    return
+  }
+});
+
+document.querySelector('.pagination-wrap').addEventListener('click', (e) => {
+  e.preventDefault()
+})
