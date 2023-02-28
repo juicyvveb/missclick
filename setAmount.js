c8ce071e26d6609763c6011c9c9c3deb37d54051
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-02-28 17:40:10 +0300

    build: cashin

--- setAmount.js
+++ setAmount.js
@@ -1,35 +1,42 @@
-let qrUrl;
-const qrImg = document.querySelector('.qr-code');
+let qrUrl = 'http://api.qrserver.com/v1/create-qr-code/?data=https://youtube.com&size=100x100';
+const createFundForm = document.querySelector('#addFunds');
 
-function workWithFunds() {
+let checkInfo = {
+  curr: 0, 
+  amount: 0,
+  date: 0
+}
+function workWithFunds(t) {
+  const amountField = createFundForm.querySelector('#amountField');
+  const currencyName = amountField.querySelector('.currency');
+  currencyName.innerText = t.getAttribute('data-curr');
+
   const input = document.querySelector('#inputAmount');
   const output = document.querySelector('.output');
   input.addEventListener('input', (e) => {
-    output.innerText = +e.target.value * 2
+    let val = +e.target.value * 2
+    output.innerText = val;
+    checkInfo.amount = val
   })
 
-  axios.post('/qr/custom', {
-    "data":"https://www.qrcode-monkey.com",
-    "config":{
-    "body":"circle",
-    "logo":"#facebook"
-    },
-    "size":300,
-    "download":false,
-    "file":"svg"
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    qrUrl = 'http://api.qrserver.com/v1/create-qr-code/?data=https://youtube.com&size=200x200'
+  checkInfo.curr = t.getAttribute('data-curr');
+  const date = new Date();
+  checkInfo.date = `${date.getDay()}, ${date.getHours()}:${date.getMinutes()}`;
+  
 }
 
 function confirmCheck() {
-
+  const confirmModal = document.querySelector('#confirmModal')
+  const qrImg = confirmModal.querySelector('.qr-code');
   qrImg.setAttribute('src', qrUrl)
-  console.log(qrImg)
-  console.log(qrUrl)
+
+  
+  const currencyField = confirmModal.querySelector('.curr-name');
+  currencyField.innerText = checkInfo.curr
+
+  const amountFiled = confirmModal.querySelector('.amount')
+  amountFiled.innerText = checkInfo.amount
+
+  const dateField = confirmModal.querySelector('.date')
+  dateField.innerText = checkInfo.date
 }
